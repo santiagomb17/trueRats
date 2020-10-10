88e58279cf64fa26ea143b8e66ca16254264f04e
santiagomb17 <60156047+santiagomb17@users.noreply.github.com>
2020-10-10 12:57:51 -0500

    Add files via upload

--- Plague Rats/scripts/CollisionShape2D.gd
+++ Plague Rats/scripts/CollisionShape2D.gd
@@ -7,69 +7,40 @@ extends CollisionShape2D
 #   ../nodeName -> child of parent node
 #   etc, etc
 
-onready var validNodeCollider #this is a gameObject  
-onready var validClick = false
-onready var pos2D = get_node("../Position2D")   #the ../ works just like a directory in LINUX
-onready var playerObject = get_node("../../player")
-onready var currNode = get_node("..")
-onready var endLevelNode = load("res://scenes/nodes/special nodes/goal node.tscn")
-onready var endSpawn = get_node("../../endSpawn")
+#onready var validNodeCollider #this is a gameObject  
+#onready var validClick = false
+onready var pos2D = get_node("../Position2D")         #getting position for the player to spawn at when clicked
+onready var playerObject = get_node("../../player")   #getting the player object
+onready var currNode = get_node("..")                 #reference to the current node
+#onready var endLevelNode = load("res://scenes/nodes/special nodes/goal node.tscn")
+#onready var endSpawn = get_node("../../endSpawn")
 
 signal spawnEndLvl
    #happens whenever button is clicked
 func _on_Button_pressed():
-	if validClick:
-		print("VALID CLICK")
-		   #pos2D needs to be global otherwise it'll move playerObject by relative position
-		playerObject.position = pos2D.global_position
-		   #needed to prevent double clicks
-		   #double clicks aren't game breaking, but would want to avoid them
-		#validClick = false
+	var horiz = abs(playerObject.global_position.x - pos2D.global_position.x)
+	var vert = abs(playerObject.global_position.y - pos2D.global_position.y)
+	
+	   #if on the same vert/hori level and not diagonal
+	if (horiz <= 50 || vert <= 50) && (horiz == 0 || vert == 0):
+		playerObject.global_position = pos2D.global_position
 
    #function executes whenever an area2D LEAVES the collider  
 func _on_Area2D_area_exited(area):
-	   #since this is a generic script it needs to check if each raycast exists as a child
-	if area.is_in_group("player"):
-		if currNode.has_node("upCast"):
-			currNode.get_node("upCast").enabled = false
-		if currNode.has_node("rightCast"):
-			currNode.get_node("rightCast").enabled = false
-		if currNode.has_node("downCast"):
-			currNode.get_node("downCast").enabled = false
-		if currNode.has_node("leftCast"):
-			currNode.get_node("leftCast").enabled = false
-			
-	#validClick = false
-	
+	   pass
 
    #function executes whenever an area2D ENTERS the collider  
 func _on_Area2D_area_entered(area):
-	#validClick = false
-	   #since this is a generic script it needs to check if each raycast exists as a child
-	if area.is_in_group("player"):
-		if currNode.has_node("upCast"):
-			currNode.get_node("upCast").enabled = true
-			
-		if currNode.has_node("rightCast"):
-			currNode.get_node("rightCast").enabled = true
-			
-		if currNode.has_node("downCast"):
-			currNode.get_node("downCast").enabled = true
-			
-		if currNode.has_node("leftCast"):
-			currNode.get_node("leftCast").enabled = true
-			
+	   #if end node success state
 	if currNode.is_in_group("end level"):
 		print("CONGLATURATIONS YOU  ARE WINNAR1")
+	   #if goal node then send signal to spawn end node
 	elif currNode.is_in_group("goal node") :
+		print("ENTERD GOAL")
 		if get_node("../../Level Start Node"):
+			print("ENTERD LVL")
 			get_node("../../Level Start Node").queue_free()
 			emit_signal("spawnEndLvl")
-			
-			
-			
-			
-			
 			
 
 
--- Plague Rats/scripts/endSpawn.gd
+++ Plague Rats/scripts/endSpawn.gd
@@ -4,16 +4,18 @@ extends Position2D
 var endLvlLoader = load("res://scenes/nodes/special nodes/Level End Node.tscn")
 var endLevelNode
 
-
+   #spawns node
 func _on_CollisionShape2D_spawnEndLvl():
+	   #use call_deferred for memory purposes
 	call_deferred("deferredSpawn")
-	print("hit spawnEndLvl")
 	
-	
+
 func deferredSpawn():
+	   #instance node
 	endLevelNode = endLvlLoader.instance()
+	   #add as child of the root
 	get_node("..").add_child(endLevelNode)
+	   #set position to endSpawn object
 	endLevelNode.position = position
-	print("hit deferredSpawn")
 
 
