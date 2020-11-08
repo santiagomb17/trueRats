a4512bfb0af778da9499d0fff68df918fda3ddcc
santiagomb17 <60156047+santiagomb17@users.noreply.github.com>
2020-11-08 12:46:39 -0600

    script housekeeping
    -commented a few things
    -separated move_script into several other scripts
    +goal_node script
    +end_level script
    -removed redundant code

--- Plague Rats/scripts/end_level.gd
+++ Plague Rats/scripts/end_level.gd
@@ -1,27 +1,25 @@
 extends CollisionShape2D
 
-#NODE GLOSSARY
-#   . -> current node    
-#   .. -> parent of current node
-#   $nodeName -> child of current node
-#   ../nodeName -> child of parent node
-#   etc, etc
 
 
 onready var pos2D = get_node("../Position2D")         #getting position for the player to spawn at when clicked
 onready var playerObject = get_node("../../player")   #getting the player object
-onready var currNode = get_node("..")                 #reference to the current node
 
-   #button press handles player postion
-   #LAST UPDATED 10/27/2020
+
+   #this method handles player movement
+   #LAST UPDATED 11/8/2020
 func _on_Button_pressed():
 	var horiz = abs(playerObject.global_position.x - pos2D.global_position.x)
 	var vert = abs(playerObject.global_position.y - pos2D.global_position.y)
 	
 	   #if on the same vert/hori level and not diagonal 9/21/2020
 	   #changing so you just have to be on the same x/y-axis 10/27/2020
-	#if (horiz <= 50 && vert <= 50) && (horiz == 0 || vert == 0):
 	if horiz == 0 || vert == 0:
 		playerObject.global_position = pos2D.global_position
+		
 
 
+   #this method will handle the level transition
+   #LAST UPDATED 11/8/2020
+func _on_Level_End_Node_area_entered(_area):
+	print("CONGLATURATIONS YOU ARE WINNAR1")
--- Plague Rats/scripts/interact_node.gd
+++ Plague Rats/scripts/interact_node.gd
@@ -4,34 +4,34 @@ extends CollisionShape2D
    #in the editor to change distance
    #needed to interact with objects
    #in the environment
-export(float) var interDist
-export(Array, NodePath) var hidPath
-onready var hiddenNode = []# = [get_node(hidPath[0])]
+export(float) var interDist           #minimum distance the player can be from
+									  #the interact node to use it
+export(Array, NodePath) var hidPath   #array of NodePaths
+onready var hiddenNode = []           #array of Objects
 onready var playerObject = get_node("../../player")
 onready var pos2D = get_node("../Position2D")
 
 
-# Called when the node enters the scene tree for the first time.
+   #populates and hides nodes from editor
+   #LAST UPDATED 11/8/2020
 func _ready():
-	#interDist = global_position
+	   #for all the objects in hidPath append to hiddenNode and hide from player
 	for i in hidPath.size():
-		#hiddenNode[i] = get_node(hidPath[i])
 		hiddenNode.append(get_node((hidPath[i])))
-		pass
-	
-	for i in hiddenNode.size():
 		hiddenNode[i].visible = false
-	#print(hidPath)
-	pass
 
 
 
+   #this method handles unhiding nodes
+   #LAST UPDATED 11/8/2020
 func _on_Button_pressed():
+	   #getting horizontal distance 
 	var horiz = abs(playerObject.global_position.x - pos2D.global_position.x)
+	   #getting vertical distance 
 	var vert = abs(playerObject.global_position.y - pos2D.global_position.y)
+	   #if the player is in interDist (from editor) AND on parallel
 	if (horiz <= interDist && vert <= interDist) && (horiz == 0 || vert == 0):
+		   #show every hidden node
 		for i in hiddenNode.size():
 			hiddenNode[i].visible = true
-		print("Enterd ReVis")
-	pass # Replace with function body.
 
