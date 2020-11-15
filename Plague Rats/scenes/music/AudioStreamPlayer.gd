25c2658ce988d969ae54fb037606c72f4bff4417
santiagomb17 <60156047+santiagomb17@users.noreply.github.com>
2020-11-15 13:08:52 -0600

    Santiago: fiddled with sound fading

--- Plague Rats/scenes/music/AudioStreamPlayer.gd
+++ Plague Rats/scenes/music/AudioStreamPlayer.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends AudioStreamPlayer
 
 
 # Declare member variables here. Examples:
@@ -8,9 +8,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#$AudioStreamPlayer.volume_db -=30
 	pass # Replace with function body.
 
-func _process(_delta):
-	if $AudioStreamPlayer.playing == false:
-		$AudioStreamPlayer.play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
