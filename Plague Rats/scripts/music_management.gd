f016d5e66ad9679e1e6182daf1e2d47383970ccb
santiagomb17 <60156047+santiagomb17@users.noreply.github.com>
2020-11-16 18:54:15 -0600

    Santiago: more sound fiddling

--- Plague Rats/scripts/music_management.gd
+++ Plague Rats/scripts/music_management.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#$AudioStreamPlayer.volume_db -=30
+	
+	$fader.play("fade in")
+	$AudioStreamPlayer.play()
 	pass # Replace with function body.
 
-func _process(_delta):
-	if $AudioStreamPlayer.playing == false:
-		$AudioStreamPlayer.play()
+func fade_out():
+	
+	pass
+
+
+
+#func _process(_delta):
+#	if $AudioStreamPlayer.playing == false:
+#		$AudioStreamPlayer.play()
