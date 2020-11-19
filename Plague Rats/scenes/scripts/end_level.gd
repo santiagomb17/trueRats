6fca1319ee7045b319ad57f20f543bde9fabea08
angelaraiza <54587383+angelaraiza@users.noreply.github.com>
2020-11-19 16:35:04 -0600

    level

--- Plague Rats/scenes/scripts/end_level.gd
+++ Plague Rats/scenes/scripts/end_level.gd
@@ -22,13 +22,13 @@ func _on_Button_pressed():
 	Global.level2_unlocked = true
 	
 
-<<<<<<< Updated upstream:Plague Rats/scripts/end_level.gd
+
 
    #this method will handle the level transition
    #LAST UPDATED 11/8/2020
 func _on_Level_End_Node_area_entered(_area):
 	playerObject.state = playerObject.FIN
 	print("CONGLATURATIONS YOU ARE WINNAR1")
-=======
+
 	print("CONGLATURATIONS YOU ARE WINNAR")
->>>>>>> Stashed changes:Plague Rats/scenes/scripts/end_level.gd
+
