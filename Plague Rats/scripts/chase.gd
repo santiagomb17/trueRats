bb3c4909144fe81b39405fe9c6ec4ea88c0a5bed
santiagomb17 <60156047+santiagomb17@users.noreply.github.com>
2020-11-21 16:28:55 -0600

    chase scene 99% ready

--- Plague Rats/scripts/chase.gd
+++ Plague Rats/scripts/chase.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var rngSeed = RandomNumberGenerator.new()
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -11,53 +12,54 @@ onready var player = get_node("player")
 func _ready():
 	select_threat()
 
-
-
-
 func select_threat():
-	print(player.state)
+	rngSeed.randomize()
+	#print(player.state)
 	if player.state != player.DEAD:
-		$"timer holder".get_child(randi() % $"timer holder".get_child_count()).start()
+		$"timer holder".get_child(rngSeed.randi() % $"timer holder".get_child_count()).start()
 	if player.state == player.DEAD:
-		print("DISPLAY DEATH MENU")
-
+		get_tree().change_scene("res://scenes/level_select_.tscn")
 
 func _on_top_button_pressed():
 	var button = get_node("button holder/top button")
 	var timer = get_node("timer holder/top timer/top")
-	button.hide()
-	timer.stop()
-	
-
+	freePress(button, timer)
 
 func _on_right_button_pressed():
 	var button = get_node("button holder/right button")
 	var timer = get_node("timer holder/right timer/right")
-	button.hide()
-	timer.stop()
-
+	freePress(button, timer)
 
 func _on_bot_buttton_pressed():
 	var button = get_node("button holder/bot button")
 	var timer = get_node("timer holder/bot timer/bot")
-	button.hide()
-	timer.stop()
-	
-
+	freePress(button, timer)
 
 func _on_bot_timeout():
 	print("DEAD")
 	player.state = player.DEAD
-	pass # Replace with function body.
-
+	###spawn menu
+	
+	###spawn menu
 
 func _on_right_timeout():
 	print("DEAD")
 	player.state = player.DEAD
-	pass # Replace with function body.
-
+	###spawn menu
+	#get_tree().change_scene("res://scenes/level_select_.tscn")
+	###spawn menu
 
 func _on_top_timeout():
 	print("DEAD")
 	player.state = player.DEAD
-	pass # Replace with function body.
+	###spawn menu
+	#get_tree().change_scene("res://scenes/level_select_.tscn")
+	###spawn menu
+
+func freePress(button, timer):
+	button.hide()
+	timer.stop()
+
+func _on_winLimit_timeout():
+	print("SUCCESSFUL CHASE")
+	get_tree().change_scene("res://scenes/level_select_.tscn")
--- Plague Rats/scripts/sub timer.gd
+++ Plague Rats/scripts/sub timer.gd
@@ -1,5 +1,6 @@
 extends Timer
 
+var rngSeed = RandomNumberGenerator.new()
 
 onready var base = get_node("../../..")
 onready var player = get_node("../../../player")
@@ -22,23 +23,26 @@ func _ready():
 func _on_top_timer_timeout():
 	#print("TOP")
 	#var button = get_node("../../../button holder/top button")
+	rngSeed.randomize()
 	button1.show()
 	base.select_threat()
-	start(2)
+	start(rngSeed.randf()*5.0+2.0)
 
 
 func _on_right_timer_timeout():
 	#print("RIGHT")
 	#var button = get_node("../../../button holder/right button")
+	rngSeed.randomize()
 	button2.show()
 	base.select_threat()
-	start(2)
+	start(rngSeed.randf()*5.0+2.0)
 
 
 func _on_bot_timer_timeout():
 	#print("BOT")
 	#var button = get_node("../../../button holder/bot button")
+	rngSeed.randomize()
 	button3.show()
 	base.select_threat()
-	start(2)
+	start(rngSeed.randf()*5.0+2.0)
 
