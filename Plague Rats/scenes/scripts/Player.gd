b9a96c5cf4fcc5f8a234b58f4258b9660fb6c6c0
matthew-luu <72053684+matthew-luu@users.noreply.github.com>
2020-12-01 13:43:52 -0600

    Costume Implimentation
    Please link your animations to the player script.

--- Plague Rats/scenes/scripts/Player.gd
+++ Plague Rats/scenes/scripts/Player.gd
@@ -7,18 +7,47 @@ var nextpos = null
 var startpos = null
 var runtime = 0
 
+var costume = 0 #Set to 0 for base skin
+var idle = "Idle"
+var run = "Run"
+
+func _ready():
+	match costume:
+		1:
+			idle = "Sombrero Idle"
+			run = "Sombrero Run"
+		2:
+			idle = "Chef Idle"
+			run = "Chef Run"
+		3:
+			idle = "Idle"
+			run = "Run"
+		4:
+			idle = "Idle"
+			run = "Run"
+		5:
+			idle = "Idle"
+			run = "Run"
+		6:
+			idle = "Idle"
+			run = "Run"
+		_:
+			idle = "Idle"
+			run = "Run"
+	$AnimatedSprite.play(idle) #set the initial animation with the costume before each level.
+
 #Added 11/25 by Matthew
 #Used to animate the player sprite
 func _process(delta):
 	if(!nextpos == null && state != DEAD):
 		runtime += .01
-		$AnimatedSprite.play("Run")
+		$AnimatedSprite.play(run)
 		position = startpos.linear_interpolate(nextpos,runtime/1.5)
 		if(position == nextpos):
-			$AnimatedSprite.play("Idle")
+			$AnimatedSprite.play(idle)
 			startpos = null
 			nextpos = null
 			runtime = 0
 		if(state == DEAD):
-			$AnimatedSprite.play("Idle")
+			$AnimatedSprite.play("Dead")
 
