2d3e554d206106f05e521336e8a848b875bf4cad
mem0600 <melanie.martinez4@mavs.uta.edu>
2020-12-01 17:42:23 -0600

    Costumes

---
+++ Plague Rats/scenes/Costumes/WhiteButton.gd
@@ -0,0 +1,15 @@
+extends Button
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if Score.get_chedda_score() == 2:
+	return 
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
