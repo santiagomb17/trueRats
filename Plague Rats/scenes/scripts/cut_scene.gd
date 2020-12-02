4ee3684ccbf785a889d8e4458cddc42a2016877a
santiagomb17 <60156047+santiagomb17@users.noreply.github.com>
2020-12-01 23:13:22 -0600

    cut_scene

--- Plague Rats/scenes/scripts/cut_scene.gd
+++ Plague Rats/scenes/scripts/cut_scene.gd
@@ -1,18 +1,6 @@
 extends Node2D
 
 
-
-func prog_start():
+func _ready():
+	$AnimationPlayer.play("flag")
 	pass
-	
-func art_start():
-	pass
-	
-
-func sound_start():
-	pass
-	
-
-func ret_vis():
-	pass
-	
